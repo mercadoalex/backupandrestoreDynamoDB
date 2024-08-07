90b570a9410e034304e6a72094b09a575f8c99f7
mercadoalex <mercadoalex@gmail.com>
2024-08-07 15:56:12 -0600

    restore_from_backups_parallel.sh and restore_from_backups_deleteifexits.sh scripts

--- scripts/restore_from_backups_deleteifexits.sh
+++ scripts/restore_from_backups_deleteifexits.sh
@@ -10,6 +10,20 @@ check_table_exists() {
   aws dynamodb describe-table --region "$AWS_REGION" --table-name "$table_name" > /dev/null 2>&1
 }
 
+# Function to delete a table
+delete_table() {
+  local table_name=$1
+  echo "Deleting table $table_name..."
+  aws dynamodb delete-table --region "$AWS_REGION" --table-name "$table_name"
+  echo "Table $table_name deleted."
+}
+
+# Function to check the status of a table
+check_table_status() {
+  local table_name=$1
+  aws dynamodb describe-table --region "$AWS_REGION" --table-name "$table_name" --query "Table.TableStatus" --output text
+}
+
 # Function to restore a table from a backup
 restore_table() {
   local backup_table_name=$1
@@ -32,11 +46,19 @@ restore_table() {
     aws dynamodb restore-table-from-backup --region "$AWS_REGION" --target-table-name "$original_table_name" --backup-arn "$backup_table_name"
     echo "Restored table $original_table_name."
   fi
+
+  # Check the status of the restored table
+  echo "Checking status of table $original_table_name..."
+  local status
+  status=$(check_table_status "$original_table_name")
+  echo "Table $original_table_name status: $status"
 }
 
 export -f check_table_exists
+export -f delete_table
+export -f check_table_status
 export -f restore_table
 export AWS_REGION
 
 # List all tables and filter those with the -backup suffix, then restore them in parallel
-aws dynamodb list-tables --region "$AWS_REGION" --query "TableNames[]" --output text | tr '\t' '\n' | grep '-backup$' | xargs -n 1 -P 4 -I {} bash -c 'restore_table "$@"' _ {}
+aws dynamodb list-tables --region "$AWS_REGION" --query "TableNames[]" --output text | tr '\t' '\n' | grep '\-backup$' | xargs -n 1 -P 4 -I {} bash -c 'restore_table "$@"' _ {}
